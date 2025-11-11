2f177cc60d82f03d31bc4c8a917e14e93330cb22
Shamiul40 <shamiul4040@gmail.com>
2025-11-12 00:33:31 +0600

    task board added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import './App.css'
-import Header from './Header'
+import "./App.css";
+import Footer from "./Footer";
+
+import Header from "./Header";
+import Hero from "./Hero";
+import TaskBoard from "./Task/TaskBoard";
 
 function App() {
- 
-
   return (
     <>
-   <Header />
+      <Header />
+      <div className="flex flex-col justify-center items-center">
+        <Hero />
+        <TaskBoard />
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
