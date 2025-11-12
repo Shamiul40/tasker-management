6151e6716c847e2f2a63653a2c33205c1d180f35
Shamiul40 <shamiul4040@gmail.com>
2025-11-12 11:17:40 +0600

    addtask modal to the ui

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -2,34 +2,33 @@ import React, { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
+import AddModalTask from "./AddModalTask";
 
 const allTask = [
   {
-    "id" : crypto.randomUUID(),
-    "title" : "Integration API",
-    "description" : "Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
-    "tags" : ["web", "python", "api"],
-    "priority" : "High",
-    "isFavourite" : true
-  }
-]
+    id: crypto.randomUUID(),
+    title: "Integration API",
+    description:
+      "Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
+    tags: ["web", "python", "api"],
+    priority: "High",
+    isFavourite: true,
+  },
+];
 
 export default function TaskBoard() {
-
   const [tasks, setTasks] = useState(allTask);
-
-  const handleAddTask =()=>{
-    console.log("clicked")
-  }
+  const [showModalTask, setShowModalTask] = useState(false);
 
   return (
     <section className="mb-20" id="tasks">
+       {showModalTask && (<AddModalTask />)}
       <div className="container">
         <SearchTask />
-
+       
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction handleAddTask={handleAddTask} />
-          <TaskList tasks={tasks}/>
+          <TaskAction handleAddTask={() => setShowModalTask(true)} />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
